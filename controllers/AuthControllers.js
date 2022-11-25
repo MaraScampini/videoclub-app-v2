4e94dfb07d9ca62ba297796628ea920ca7bbb4ac
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-11-25 14:08:38 +0100

    Changes

--- controllers/AuthControllers.js
+++ controllers/AuthControllers.js
@@ -1,14 +1,14 @@
-const models = require('../models/index');
+const models = require("../models/index");
 const {
   assertValidPasswordService,
   assertEmailIsValidService,
   assertEmailIsUniqueService,
   createUserService,
-  encryptPasswordService
-} = require('../Services/AuthServices');
-require('dotenv').config();
+  encryptPasswordService,
+} = require("../Services/AuthServices");
+require("dotenv").config();
 
-const jsonwebtoken = require('jsonwebtoken');
+const jsonwebtoken = require("jsonwebtoken");
 
 const authRegisterController = async (req, res) => {
   const body = req.body;
@@ -18,7 +18,7 @@ const authRegisterController = async (req, res) => {
     assertValidPasswordService(body.password);
   } catch (error) {
     console.error(error);
-    res.status(400).send(`Invalid password, ${error.message}`)
+    res.status(400).send(`Invalid password, ${error.message}`);
     return;
   }
   // Chech that the email has a valid structure
@@ -26,48 +26,49 @@ const authRegisterController = async (req, res) => {
     assertEmailIsValidService(body.email);
   } catch (error) {
     console.error(error);
-    res.status(400).send(`Invalid email, ${error.message}`)
+    res.status(400).send(`Invalid email, ${error.message}`);
     return;
   }
   // Check that the email is not already registered
   try {
-   await assertEmailIsUniqueService(body.email);
+    await assertEmailIsUniqueService(body.email);
+    console.log("Hola");
   } catch (error) {
     console.error(error);
-    res.status(400).send(`Email is already registered`)
+    res.status(400).send(`Email is already registered`);
     return;
   }
   // Create the new user
   try {
     const UserCreated = await createUserService(body);
-    res.status(201).json(UserCreated)
+    res.status(201).json(UserCreated);
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: error.message })
+    console.error(error);
+    res.status(500).json({ message: error.message });
   }
-}
+};
 
 const authLoginController = async (req, res) => {
   const { email, password } = req.body;
   try {
     // Find the user by their email
     const userFound = await models.Users.findOne({
-      where: { email: email }
+      where: { email: email },
     });
     if (!userFound) {
       res.status(401).json({ message: "Password or email incorrect" });
       return;
     }
 
-    if(userFound.deleted == true) {
+    if (userFound.deleted == true) {
       res.status(401).json({ message: "Access forbidden" });
       return;
     }
     // Encrypt the provided password and check that it matches with the hash in the database
-    const hashedPassword = encryptPasswordService(password)
+    const hashedPassword = encryptPasswordService(password);
 
     if (hashedPassword !== userFound.password) {
-      res.status(401).json({ message: "Password or email incorrect" })
+      res.status(401).json({ message: "Password or email incorrect" });
       return;
     }
     // Create a JSON Web Token and give it to the user
@@ -77,22 +78,25 @@ const authLoginController = async (req, res) => {
       throw new Error("JWT_SECRET is not set");
     }
 
-    const jwt = jsonwebtoken.sign({
-      email: userFound.email,
-      id: userFound.id_user,
-      role: userFound.RoleIdRole.toLowerCase()
-    }, secret);
+    const jwt = jsonwebtoken.sign(
+      {
+        email: userFound.email,
+        id: userFound.id_user,
+        role: userFound.RoleIdRole.toLowerCase(),
+        name: userFound.name,
+      },
+      secret
+    );
     res.status(200).json({
       message: "Login successful",
-      your_token_is: jwt
-    })
+      jwt: jwt,
+    });
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
-
-}
+};
 
 module.exports = {
   authLoginController,
-  authRegisterController
+  authRegisterController,
 };
