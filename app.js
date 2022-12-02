7d316a651c50fae2f38ad0800e7efa106b50c133
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-02 16:30:50 +0100

    config

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./db/db");
+const db = require("./db/db.js");
 const { sequelize } = require("./models/index");
 const app = express();
 const router = require("./router");
